1797b9d4f5526762bfa02069c9a1bf0c8f184831
IMIVSH <iamivanshumilin@gmail.com>
2022-12-26 17:59:18 +0300

    Task10 done

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите ваше трёхзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int count = number;
+int lengthCounter = 0;
+
+    while (count > 0)
+{
+    lengthCounter++;
+    count = count/10;
+}
+
+if (lengthCounter != 3)
+{
+    Console.WriteLine("Вы ввели неверное число!");
+}
+else
+{
+    number = number / 10;
+    number = number % 10;
+    Console.WriteLine(number);
+}
